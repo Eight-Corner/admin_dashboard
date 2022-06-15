1ec01c5d40a78c2a26688aad7dc39bb1c44a295f
Eight-Corner <corner3499@gmail.com>
2022-06-15 12:59:17 +0900

    logo text

--- pages/Auth.vue
+++ pages/Auth.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="auth_page" class="app-page">
 
-        <div id="logo">ADMIN</div>
+        <div id="logo" class="logo-text">
+            ADMIN
+        </div>
 
         <div class="b-t">
             <div class="content-wrap center-block p-y-md text-center">
@@ -29,6 +31,33 @@ export default {
 
 <style scoped>
 
+.logo-text {
+    text-transform: uppercase;
+    background-image: linear-gradient(
+        -225deg,
+        #231557 0%,
+        #44107a 29%,
+        #ff1361 67%,
+        #fff800 100%
+    );
+    background-size: auto auto;
+    background-clip: border-box;
+    background-size: 200% auto;
+    color: #fff;
+    background-clip: text;
+    text-fill-color: transparent;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    animation: textclip 2s linear infinite;
+    display: inline-block;
+    font-size: 56px;
+}
+
+@keyframes textclip {
+    to {
+        background-position: 200% center;
+    }
+}
 
 #logo {
     width: 200px;
