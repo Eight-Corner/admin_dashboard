b48ecbbfd1b25f885a1d762ff8330b7480096850
corner <corner3499@gmail.com>
2022-06-15 23:03:45 +0900

    회원가입 폼 완료

--- pages/Auth.vue
+++ pages/Auth.vue
@@ -9,11 +9,12 @@
             <div class="content-wrap center-block p-y-md text-center">
                 <div class="p-a-md">
                     <div class="social_btn_wrap">
+                        <div class="text-white mb15">SNS 계정으로 간편 로그인하기 </div>
                         <el-button href="#" class="btn btn-block red text-white google">
                             <i class="fa fa-google-plus pull-left"></i>Sign in with Google+
                         </el-button>
                     </div>
-                    <div class="mt20 mb20 text-sm">
+                    <div class="mt20 mb20 text-sm b-b b-t p-a-md">
                         <span class="text-white">또는</span>
                     </div>
                     <AuthLoginComponent />
@@ -39,54 +40,5 @@ export default {
 
 <style scoped>
 
-.logo-text {
-    text-transform: uppercase;
-    background-image: linear-gradient(
-        -225deg,
-        #231557 0%,
-        #44107a 29%,
-        #ff1361 67%,
-        #fff800 100%
-    );
-    background-size: auto auto;
-    background-clip: border-box;
-    background-size: 200% auto;
-    color: #fff;
-    background-clip: text;
-    text-fill-color: transparent;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    animation: textclip 2s linear infinite;
-    display: inline-block;
-    font-size: 56px;
-}
-
-@keyframes textclip {
-    to {
-        background-position: 200% center;
-    }
-}
-
-#logo {
-    width: 200px;
-    height: 200px;
-    padding: 1.5rem;
-    display: flex;
-    justify-content: center;
-    align-content: center;
-    align-items: center;
-
-    margin: 0 auto;
-}
-
-.content-wrap {
-    /* 로그인 폼 validate 때문에 최저너비 수정*/
-    min-width: 355px;
-    max-width: 400px;
-}
-
-.google {
-    border-color: #ff4336;
-}
 
 </style>
